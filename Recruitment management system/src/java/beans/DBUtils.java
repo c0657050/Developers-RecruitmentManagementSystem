3f3bf6a4e9289776c1c98ed94f5a9dc0a7e28b19
c0667381 <c0667381@N208-F15-011.lambton.on.ca>
2016-04-14 01:40:04 -0400

    Job seeker pages and DBUtils created.

---
+++ Recruitment management system/src/java/beans/DBUtils.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author c0667381
+ */
+public class DBUtils {
+    
+     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+        String host = "localhost";
+        String port = "3306";
+        String db = "rms";
+        String user = "root";
+        String pass = "";
+        String jdbc = "jdbc:mysql://" + host + ":" + port + "/" + db;
+        return DriverManager.getConnection(jdbc, user, pass);
+    }
+
+    public final static String SALT = "THISISArandomSTRINGofCHARACTERSusedTOsaltTHEpasswords";
+
+    public static String hash(String password) {
+        try {
+            String salted = password + SALT;
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] hash = md.digest(salted.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(b & 0xff).toUpperCase();
+                if (hex.length() == 1) {
+                    sb.append("0");
+                }
+                sb.append(hex);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+   
+}
